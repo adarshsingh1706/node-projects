9201109946dbcc1df3d9bf3f8a22f12618af1fd8
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-04-10 22:40:05 +0530

    get/:shortid

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
 app.use(express.json());
 app.use("/url", urlRoute);
 
-app.get("./:shortId", async (req, res) => {
+app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
-      shortId,
+      shortId, //find
     },
     {
       $push: {
